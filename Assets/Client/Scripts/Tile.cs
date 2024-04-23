a9e72c47f5af9ae42da288b6539e495407cd5d35
Nastia <adruts@mail.ru>
2024-04-24 01:58:58 +0300

    https://github.com/anastyadru/Match-3.git
    Отредактировала скрипт Tile

--- Assets/Client/Scripts/Tile.cs
+++ Assets/Client/Scripts/Tile.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Tile : MonoBehaviour
+public sealed class Tile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int x;
+    public int y;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Item item;
+
+    public Image icon;
+
+    public Button button;
 }
