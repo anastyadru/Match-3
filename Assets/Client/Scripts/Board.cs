c7aa9f66fdbdd41480fdaf3e83cdce63b3be8611
Nastia <adruts@mail.ru>
2024-04-24 16:31:56 +0300

    https://github.com/anastyadru/Match-3.git
    Добавила скрипт Board

--- Assets/Client/Scripts/Board.cs
+++ Assets/Client/Scripts/Board.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Board : MonoBehaviour
+public sealed class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static Board Instance { get; private set; }
+    public Row[] rows;
+    publiclic Tile[,] Tiles { get; private set; }
 }
