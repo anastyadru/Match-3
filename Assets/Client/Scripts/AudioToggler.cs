66397dc60e9e50aec73a461480d52a217af8091b
Nastia <adruts@mail.ru>
2024-05-01 15:38:17 +0300

    https://github.com/anastyadru/Match-3.git
    Отредактировала скрипт AudioToggler

--- Assets/Client/Scripts/AudioToggler.cs
+++ Assets/Client/Scripts/AudioToggler.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class AudioToggler : MonoBehaviour
 {
+    public bool isOn;
+    
+    private void Start()
+    {
+        isOn = PlayerPrefs.GetInt("SoundOn", 1) == 1;
+        if (isOn)
+        {
+            AudioListener.volume = 1f;
+        }
+        else
+        {
+            AudioListener.volume = 0f;
+        }
+    }
+    
+    public void OnOffSounds()
+    {
+        if (!isOn)
+        {
+            AudioListener.volume = 1f;
+            isOn = true;
+        }
+        else
+        {
+            AudioListener.volume = 0f;
+            isOn = false;
+        }
 
+        PlayerPrefs.SetInt("SoundOn", isOn ? 1 : 0);
+        PlayerPrefs.Save();
+    }
 }
