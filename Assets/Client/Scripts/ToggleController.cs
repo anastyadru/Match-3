56fabf7ad337f010a2e4afa72fe6d6942550e083
Nastia <adruts@mail.ru>
2024-05-02 18:56:55 +0300

    https://github.com/anastyadru/Match-3.git
    Отредактировала скрипт ToggleController

--- Assets/Client/Scripts/ToggleController.cs
+++ Assets/Client/Scripts/ToggleController.cs
@@ -1,17 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class ToggleController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void ToggleMusic(bool enabled)
     {
         
     }
