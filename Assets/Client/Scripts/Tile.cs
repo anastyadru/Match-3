3a1a79264ba90fdaeee5490aef46ceb250fff6ae
Nastia <adruts@mail.ru>
2024-05-08 17:26:59 +0300

    https://github.com/anastyadru/Match-3.git
    Отредактировала скрипт Tile

--- Assets/Client/Scripts/Tile.cs
+++ Assets/Client/Scripts/Tile.cs
@@ -29,7 +29,9 @@ public sealed class Tile : MonoBehaviour
 	public Tile Left => x > 0 ? Board.Instance.Tiles[x - 1, y] : null;
 	public Tile Top => y > 0 ? Board.Instance.Tiles[x, y - 1] : null;
 	public Tile Right => x < Board.Instance.Width - 1 ? Board.Instance.Tiles[x + 1, y] : null;
-	
+	public Tile Bottom => y < Board.Instance.Heigh - 1 ? Board.Instance.Tiles[x, y + 1] : null;
+
+
 
     private void Start() => button.onClick.AddListener(() => Board.Instance.Select(this));
 }
