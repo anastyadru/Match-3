a0d2a3582a830c363787865192925d249c2d108e
Nastia <adruts@mail.ru>
2024-07-15 22:06:09 +0300

    https://github.com/anastyadru/Match-3.git
    Отредактировала скрипт GameM

--- Assets/Client/Scripts/GameM.cs
+++ Assets/Client/Scripts/GameM.cs
@@ -9,13 +9,11 @@ using Zenject;
 
 public class GameM : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
