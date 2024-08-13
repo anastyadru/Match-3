6f3ddac23856fbb4b2252fac682e30b40b660dbb
Nastia <adruts@mail.ru>
2024-08-13 20:26:00 +0300

    https://github.com/anastyadru/Match-3.git
    Отредактировада скрипт ScoreCounter

--- Assets/Client/Scripts/ScoreCounter.cs
+++ Assets/Client/Scripts/ScoreCounter.cs
@@ -1,6 +1,19 @@
+using TMPro;
 using UnityEngine;
 
 public sealed class ScoreCounter : MonoBehaviour
 {
+    public static ScoreCounter Instance { get; private set; }
 
+    private int _score;
+
+    public int Score
+    {
+        get => _score;
+
+        set
+        {
+            
+        }
+    }
 }
