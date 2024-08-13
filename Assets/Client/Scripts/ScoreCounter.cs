9230dae7eee7709452dc5e8234b77bafdc46a5d6
Nastia <adruts@mail.ru>
2024-08-13 19:48:01 +0300

    https://github.com/anastyadru/Match-3.git
    Создала скрипт ScoreCounter

--- Assets/Client/Scripts/ScoreCounter.cs
+++ Assets/Client/Scripts/ScoreCounter.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreCounter : MonoBehaviour
+public sealed class ScoreCounter : MonoBehaviour
 {
 
 }
