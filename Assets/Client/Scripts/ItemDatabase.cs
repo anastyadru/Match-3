a3cd39ca048d813440910fa1c63b2e0e76862674
Nastia <adruts@mail.ru>
2024-08-15 00:41:18 +0300

    https://github.com/anastyadru/Match-3.git
    Отредактировада скрипт ItemDatabase

--- Assets/Client/Scripts/ItemDatabase.cs
+++ Assets/Client/Scripts/ItemDatabase.cs
@@ -6,5 +6,6 @@ public static class ItemDatabase
 {
     public static Item[] Items { get; private set; }
 
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]private static void Initialize() => Items = Resources.LoadAll<Item>("Items/");
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Initialize() => Items = Resources.LoadAll<Item>("Items/");
 }
